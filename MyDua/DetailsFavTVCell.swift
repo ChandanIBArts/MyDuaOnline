59d38ac1f9e88c4746ec00ac194cabb170e999f3
Chandan <chandanmondal@ibarts.in>
2024-03-26 15:28:58 +0530

    commit all

---
+++ MyDua/DetailsFavTVCell.swift
@@ -0,0 +1,23 @@
+//
+//  DetailsFavTVCell.swift
+//  MyDua
+//
+//  Created by IB Arts Mac on 22/03/24.
+//
+
+import UIKit
+
+class DetailsFavTVCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
---
+++ MyDua/FavouriteTVCell.swift
@@ -0,0 +1,23 @@
+//
+//  FavouriteTVCell.swift
+//  MyDua
+//
+//  Created by IB Arts Mac on 21/03/24.
+//
+
+import UIKit
+
+class FavouriteTVCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
